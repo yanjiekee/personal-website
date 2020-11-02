a50514cae1002888b86eaeb95bd4a378f84b51f8
yj_kee <keeyanjie2013@gmail.com>
2020-11-02 23:29:16 +0000

    Added (Ajax, jQuery) functions that can display a new comment onto the webpage without loading, but only tested by using a trigger event, not by POST

--- js/app.js
+++ js/app.js
@@ -1,33 +1,29 @@
+
+// Ajax syntax to select the form id = #commentform
 $(function() {
     $('#commentform').submit(handleSubmit);
 });
 
+// When the form is posted, it is captured by Ajax
 function handleSubmit() {
-  var form = $(this);
-  var data = {
-    "comment_author": form.find('#comment_author').val(),
-    "email": form.find('#email').val(),
-    "comment": form.find('#comment').val(),
-    "comment_post_ID": form.find('#comment_post_ID').val()
-  };
+    var form = $(this);
+    var data = {
+        "comment_author": form.find('#comment_author').val(),
+        "email": form.find('#email').val(),
+        "comment": form.find('#comment').val(),
+        "comment_post_ID": form.find('#comment_post_ID').val()
+    };
 
-  var socketId = getSocketId();
-  if(socketId !== null) {
-    data.socket_id = socketId;
-  }
+    postComment(data);
 
-  postComment(data);
-
-  return false;
+    return false;
 }
 
-function getSocketId() {
-  if(pusher && pusher.connection.state === 'connected') {
-    return pusher.connection.socket_id;
-  }
-  return null;
-}
-
+// Type: Specifies the type of request. (GET or POST)
+// Url: Specifies the URL to send the request to. Default is the current page
+// Data: Specifies data to be sent to the server
+// Headers: An object of additional header key/value pairs to send along with request.
+// Success, Error: Callback functions
 function postComment(data) {
     $.ajax({
        type: 'POST',
@@ -36,12 +32,15 @@ function postComment(data) {
        headers: {
          'X-Requested-With': 'XMLHttpRequest'
        },
+       // X-Requested-With: To differentiate AJAX request from normal page views. When working with an Ajax-enhanced website, it's generally a good idea to provide a regular request fallback for any core functionality of the site. AJAX is kinda a plan-b?
        success: postSuccess,
        error: postError
      });
 }
 
 function postSuccess(data, textStatus, jqXHR) {
+    // get() method: loads data from the server using a HTTP GET request.
+    // reset(): resets the values of all elements in a form
     $('#commentform').get(0).reset();
     displayComment(data);
 }
@@ -52,12 +51,12 @@ function postError(jqXHR, textStatus, errorThrown) {
 
 function displayComment(data) {
     var commentHtml = createComment(data);
-    var commentEl = $(commentHtml);
+    var commentEl = $(commentHtml); // Converting it from HTML into a jQuery object...?
     commentEl.hide();
-    var postsList = $('#posts-list');
-    postsList.addClass('has-comments');
-    postsList.prepend(commentEl);
-    commentEl.slideDown();
+    var postsList = $('#posts-list');   // Another jQuery object
+    postsList.addClass('has-comments'); // Enable 'has-comments' variable
+    postsList.prepend(commentEl);   // Add comment into the list <ol>
+    commentEl.slideDown();  // "Show" the new comment?
 }
 
 function createComment(data) {
@@ -90,26 +89,26 @@ function parseDisplayDate(date) {
     return display;
 }
 
-// To test function displayComment()
-// $(function() {
-//
-//   $(document).keyup(function(e) {
-//     e = e || window.event;
-//     if(e.keyCode === 85){
-//       displayComment({
-//         "id": "comment_1",
-//         "comment_post_ID": 1,
-//         "date":"Tue, 21 Feb 2012 18:33:03 +0000",
-//         "comment": "The realtime Web rocks!",
-//         "comment_author": "Phil Leggetter"
-//       });
-//     }
-//   });
-//
-// });
+// To test function displayComment() by pressing 'u'
+$(function() {
 
-var pusher = new Pusher(APP_KEY, {
-      cluster: APP_CLUSTER
-    });
-var channel = pusher.subscribe('comments-' + $('#comment_post_ID').val());
-channel.bind('new_comment', displayComment);
+  $(document).keyup(function(e) {
+    e = e || window.event;
+    if(e.keyCode === 85){
+      displayComment({
+        "id": "comment_1",
+        "comment_post_ID": 1,
+        "date":"Tue, 21 Feb 2012 18:33:03 +0000",
+        "comment": "The realtime Web rocks!",
+        "comment_author": "Phil Leggetter"
+      });
+    }
+  });
+
+});
+
+// var pusher = new Pusher(APP_KEY, {
+//       cluster: APP_CLUSTER
+//     });
+// var channel = pusher.subscribe('comments-' + $('#comment_post_ID').val());
+// channel.bind('new_comment', displayComment);
